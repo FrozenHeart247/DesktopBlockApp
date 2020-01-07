9fb373c1a6b9bf37c3b3a42a106a0ccb3ba2c92b
FrozenHeart246 <kristalov2011@gmail.com>
2020-01-07 18:53:24 +0200

    FirstCommit

---
+++ NotVirus/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace NotVirus
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.picBox1 = new System.Windows.Forms.PictureBox();
+            this.EscapeButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picBox1
+            // 
+            this.picBox1.ErrorImage = null;
+            this.picBox1.Image = ((System.Drawing.Image)(resources.GetObject("picBox1.Image")));
+            this.picBox1.Location = new System.Drawing.Point(62, 39);
+            this.picBox1.Name = "picBox1";
+            this.picBox1.Size = new System.Drawing.Size(160, 160);
+            this.picBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBox1.TabIndex = 0;
+            this.picBox1.TabStop = false;
+            this.picBox1.MouseEnter += new System.EventHandler(this.picBox1_MouseEnter);
+            // 
+            // EscapeButton
+            // 
+            this.EscapeButton.Location = new System.Drawing.Point(380, 382);
+            this.EscapeButton.Name = "EscapeButton";
+            this.EscapeButton.Size = new System.Drawing.Size(75, 23);
+            this.EscapeButton.TabIndex = 1;
+            this.EscapeButton.Text = "Закрыть";
+            this.EscapeButton.UseVisualStyleBackColor = true;
+            this.EscapeButton.Click += new System.EventHandler(this.EscapeButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.AliceBlue;
+            this.ClientSize = new System.Drawing.Size(333, 267);
+            this.Controls.Add(this.EscapeButton);
+            this.Controls.Add(this.picBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.Text = "смэрть, да?";
+            this.TopMost = true;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picBox1;
+        private System.Windows.Forms.Button EscapeButton;
+    }
+}
+
---
+++ NotVirus/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NotVirus
+{
+    public partial class Form1 : Form
+    {
+        private Random rand = new Random();
+      
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            EscapeButton.Visible = false;
+            this.ShowInTaskbar = false;
+            this.TopMost = true;
+            this.WindowState = FormWindowState.Maximized;
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.AllowTransparency = true;
+            this.BackColor = Color.AliceBlue;
+            this.TransparencyKey = this.BackColor;
+
+        }
+
+        private void EscapeButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void picBox1_MouseEnter(object sender, EventArgs e)
+        {/*
+            PictureBox pictureBox2 = new PictureBox();
+            Point location = picBox1.Location;
+            int x = location.X;
+            location = picBox1.Location;
+            int y = location.Y + picBox1.Height + 10;
+            Point point1 = new Point(x, y);
+            picBox1.Location = point1;
+            this.Controls.Add((Control)picBox1);
+            Point point2 = new Point(this.rand.Next(this.Size.Width - 100), this.rand.Next(this.Size.Height - 100));
+            picBox1.Location = point2;
+            this.Controls.Add((Control)pictureBox2);
+            */
+            /*
+               PictureBox pictureBox1 = (PictureBox)sender;
+               PictureBox pictureBox2 = new PictureBox();
+               pictureBox2.BackColor = (sender as PictureBox).BackColor;
+               pictureBox2.Image = (sender as PictureBox).Image;
+               pictureBox2.SizeMode = (sender as PictureBox).SizeMode;
+               pictureBox2.Width = pictureBox1.Width;
+               pictureBox2.Height = pictureBox1.Height;
+               PictureBox pictureBox3 = pictureBox2;
+               Point location = pictureBox1.Location;
+               int x = location.X;
+               location = pictureBox1.Location;
+               int y = location.Y + pictureBox1.Height + 10;
+               Point point1 = new Point(x, y);
+               pictureBox3.Location = point1;
+               pictureBox2.MouseEnter += new EventHandler(this.picBox1_MouseEnter);
+               this.Controls.Add((Control)pictureBox2);
+               Point point2 = new Point(this.rand.Next(this.Size.Width - 100), this.rand.Next(this.Size.Height - 100));
+               (sender as PictureBox).Location = point2;
+           }*/
+            
+            PictureBox pictureBox2 = new PictureBox();
+            pictureBox2.BackColor = (sender as PictureBox).BackColor;
+            pictureBox2.Image = (sender as PictureBox).Image;
+            pictureBox2.SizeMode = (sender as PictureBox).SizeMode;
+            pictureBox2.Width = picBox1.Width;
+            pictureBox2.Height = picBox1.Height;
+            PictureBox pictureBox3 = pictureBox2;
+            Point location = picBox1.Location;
+            int x = location.X;
+            location = picBox1.Location;
+            int y = location.Y + picBox1.Height + 10;
+            Point point1 = new Point(x, y);
+            pictureBox3.Location = point1;
+            pictureBox2.MouseEnter += new EventHandler(picBox1_MouseEnter);
+            this.Controls.Add((Control)pictureBox2);
+            Point point2 = new Point(rand.Next(Size.Width - 100),rand.Next(Size.Height - 100));
+            pictureBox3.Location = point2;
+        }
+
+     
+    }
+}
